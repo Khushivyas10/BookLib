0716be9172e5251606d412cede30440eea65b5f9
Khushivyas10 <khushivyas1003@gmail.com>
2024-09-01 13:56:31 +0530

    test: added test for add_book function

--- test_main.py
+++ test_main.py
@@ -17,4 +17,35 @@ def setup_library():
 	yield library_instance
 	library_instance.connection.close()  # Ensure the connection is closed
 
+# checks whether a new book can be added to the library database and
+# verifies its existence before and after the addition
+def test_add_book(setup_library):
+	# Create a new Book object using the user's input
+	library = setup_library
+	new_book = Book(
+		title="test_book_title",
+		author="test_book_author",
+		publication_year=9999,
+		ISBN="9783161484100",
+	)
+	book_in_lib = library.cursor.execute(
+		"SELECT EXISTS(SELECT title FROM books WHERE title='test_book_title')"
+	).fetchone()
+	# sqlite iterator returns value in tuple.
+	assert book_in_lib == (0,)  # to ensure book does not exist before adding
+
+	library.add_book(new_book)
+	book_in_lib = library.cursor.execute(
+		"SELECT EXISTS(SELECT title FROM books WHERE title='test_book_title')"
+	).fetchone()
+	assert book_in_lib == (
+		1,
+	)  # to ensure that the book has been successfully added and exists in the database.
+
+	# Attempt to add the same book again (with the same ISBN)
+	with pytest.raises(
+		sqlite3.IntegrityError
+	):  # Expecting an IntegrityError due to the unique ISBN constraint
+		library.add_book(new_book)
+
 
