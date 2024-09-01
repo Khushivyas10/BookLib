58a67d09bf190a94d03f450e045753f9d228a258
Khushivyas10 <khushivyas1003@gmail.com>
2024-09-01 14:04:04 +0530

    feat: added library method for adding book
    misc: added method for getting books from db

--- library.py
+++ library.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-
+from book import Book
 from exception import ValidationError
 
 
@@ -16,35 +16,39 @@ class library:
 	def create_table(self):
 		self.cursor.execute(
 			"""
-            CREATE TABLE IF NOT EXISTS books (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                title TEXT NOT NULL,
-                author TEXT NOT NULL,
-                publication_year INTEGER,
-                isbn TEXT NOT NULL UNIQUE,
-                borrowed BOOLEAN NOT NULL CHECK (borrowed IN (0, 1)) DEFAULT 0
-            )
-            """
+			CREATE TABLE IF NOT EXISTS books (
+				id INTEGER PRIMARY KEY AUTOINCREMENT,
+				title TEXT NOT NULL,
+				author TEXT NOT NULL,
+				publication_year INTEGER,
+				isbn TEXT NOT NULL UNIQUE,
+				borrowed BOOLEAN NOT NULL CHECK (borrowed IN (0, 1)) DEFAULT 0
+			)
+			"""
 		)
 		self.connection.commit()
 
 	def get_books_from_db(self):
-		pass
+		"""Fetch all books from the database and return them as a list of tuples."""
+		self.cursor.execute("SELECT title, author, publication_year, isbn FROM books")
+		books = self.cursor.fetchall()
+		return books
 
-	def add_book(self, book):
-		pass
+	def add_book(self, book: Book):
+		self.cursor.execute(
+			"""
+				INSERT INTO books (title, author, publication_year, isbn, borrowed)
+				VALUES (?, ?, ?, ?, ?)
+				""",
+			(book.title, book.author, book.publication_year, book.ISBN, book.borrowed),
+		)
+		self.connection.commit()
 
 	def borrow_book(self, title):
 		pass
 
-
-
 	def return_book(self, title):
 		pass
 
-
-
 	def view_available_books(self):
 		pass
-
-
